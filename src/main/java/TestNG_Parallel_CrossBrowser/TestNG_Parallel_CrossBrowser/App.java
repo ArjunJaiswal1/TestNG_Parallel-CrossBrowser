d74b8a57ecdc8ac59f9a99da7621a60790644645
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-11-22 10:24:07 +0545

    Parallel cross browser

---
+++ src/main/java/TestNG_Parallel_CrossBrowser/TestNG_Parallel_CrossBrowser/App.java
@@ -0,0 +1,13 @@
+package TestNG_Parallel_CrossBrowser.TestNG_Parallel_CrossBrowser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
